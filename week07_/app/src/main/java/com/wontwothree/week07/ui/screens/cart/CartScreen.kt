79840a9992c96751e0476a5618445d51979f89fe
WAcAW9 <wacaw9@gmail.com>
2026-05-13 13:45:22 +0900

    feat : week07/화면 UI 구현

--- week07_/app/src/main/java/com/wontwothree/week07/ui/screens/cart/CartScreen.kt
+++ week07_/app/src/main/java/com/wontwothree/week07/ui/screens/cart/CartScreen.kt
@@ -1,7 +1,48 @@
 package com.wontwothree.week07.ui.screens.cart
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wontwothree.week07.R
+import com.wontwothree.week07.ui.theme.Week07Theme
 
 @Composable
 fun CartScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id=R.drawable.ic_bag_circle),
+            contentDescription = "장바구니 아이콘",
+            modifier = Modifier
+                .width(60.dp)
+                .height(60.dp)
+        )
+        Spacer(Modifier.height(27.dp))
+        Text("장바구니가 비어 있습니다.", fontSize = 16.sp)
+        Text("제품을 추가하면 여기에 표시됩니다.",fontSize = 16.sp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    Week07Theme(){
+        CartScreen()
+    }
 }
--- week07_/app/src/main/java/com/wontwothree/week07/ui/screens/whishlist/WhishlistScreen.kt
+++ week07_/app/src/main/java/com/wontwothree/week07/ui/screens/whishlist/WhishlistScreen.kt
@@ -1,7 +1,36 @@
 package com.wontwothree.week07.ui.screens.whishlist
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wontwothree.week07.ui.screens.home.HomeScreen
+import com.wontwothree.week07.ui.theme.Week07Theme
 
 @Composable
 fun WhishlistScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 17.dp, vertical = 50.dp)
+    ) {
+        Text("위시리스트",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    Week07Theme(){
+        WhishlistScreen()
+    }
 }
