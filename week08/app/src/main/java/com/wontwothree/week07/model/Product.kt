1ffb5fd6dbb31c4bc43684e16ecde8d4a682e958
WAcAW9 <wacaw9@gmail.com>
2026-05-19 15:26:40 +0900

    feat : week08/Product 공통 컴포넌트 구현
    - Product 모델 정의
    - 공통 상품 UI 컴포넌트 NewProductCard, ProductCard 구현

---
+++ week08/app/src/main/java/com/wontwothree/week07/model/Product.kt
@@ -0,0 +1,24 @@
+package com.wontwothree.week07.model
+
+import com.wontwothree.week07.R
+
+data class Product(
+    val id: Int,
+    val name:String,
+    val category:String,
+    val price:Int,
+    val colors:Int,
+    val imageRes:Int,
+    val isBestSeller:Boolean=false,
+    val isWishList: Boolean=false
+)
+
+val sampleProduct = listOf(
+    Product(1, "Air Jordan XXXVI", "Men's Shoes", 185, 5, R.drawable.img_product_1),
+    Product(2, "Nike Air Force 1 '07", "Men's Shoes", 115, 6, R.drawable.img_product_2),
+    Product(3, "Nike Everyday Plus Cushioned", "Training Ankle Socks (6 Pairs)", 10, 5, R.drawable.img_product_3),
+    Product(4, "Nike Elite Crew", "Basketball Socks", 16, 7, R.drawable.img_product_5),
+    Product(5, "Nike Air Force 1 '07", "Women's Shoes", 115, 5, R.drawable.img_product_4, isBestSeller = true),
+    Product(6, "Jordan ENike Air Force 1 '07ssentials", "Men's Shoes", 115, 2, R.drawable.img_product_6, isBestSeller = true),
+
+    )
--- week08/app/src/main/java/com/wontwothree/week07/ui/screens/home/HomeScreen.kt
+++ week08/app/src/main/java/com/wontwothree/week07/ui/screens/home/HomeScreen.kt
@@ -2,7 +2,6 @@ package com.wontwothree.week07.ui.screens.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
